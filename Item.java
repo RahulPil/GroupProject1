049e143b852c8ea0c671030ccbc040957dd84285
JulianMydlil <julian.mydlil@icloud.com>
2020-10-06 10:27:30 -0700

    first edits to item.java

--- Item.java
+++ Item.java
@@ -1,8 +1,27 @@
-
+//implements bulk discounts
+//constructor
 public class Item {
 
 	public Item() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
+
+/*
+    private double price; 
+	private String name;
+
+	public Item(String name, double price) {
+		this.name = name; 
+		this.price = price;
+	}
+	public double setPrice(double price) {
+		this.price = price;
+		return price;
+	}
+	public String setName(String name) {
+		this.name = name;
+		return name; 
+	}
+*/
