63af3b525bb9a338eb2ab9a434f12a5f5e211c32
MangoPicante <8892151+MangoPicante@users.noreply.github.com>
2020-10-06 12:38:01 -0700

    Fleshed out Item class with getters, setters, and additional constructors

--- Item.java
+++ Item.java
@@ -1,27 +1,45 @@
 //implements bulk discounts
 //constructor
 public class Item {
-
-	public Item() {
-		
-	}
-
-}
-
-/*
-    private double price; 
 	private String name;
-
-	public Item(String name, double price) {
+	private double price; 
+	private double discount;
+	
+	//CONSTRUCTORS
+	public Item() {
+		this("",0,0);
+	}
+    public Item(String name, double price, double discount) {
 		this.name = name; 
 		this.price = price;
+		this.discount = discount;
 	}
-	public double setPrice(double price) {
-		this.price = price;
-		return price;
-	}
-	public String setName(String name) {
+    public Item(String name, double price) {
+    	this(name,price,0);
+    }
+    
+    //SETTERS
+    public void setName(String name) {
 		this.name = name;
-		return name; 
 	}
-*/
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+	
+	//GETTERS
+	public String getName() {
+		return this.name;
+	}
+	public double getPrice() {
+		return this.price;
+	}
+	public double getDiscount() {
+		return this.discount;
+	}
+	public double getDiscountedPrice() {
+		return this.price - this.discount;
+	}
+}
