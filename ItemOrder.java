1ef54e71de8fe693462a713f2cbd333c79156718
JulianMydlil <julian.mydlil@icloud.com>
2020-10-06 10:28:12 -0700

    first edits to itemorder.java

--- ItemOrder.java
+++ ItemOrder.java
@@ -1,8 +1,15 @@
+/* 
+... also implement an ItemOrder class that represents a 
+shopper’s desire to purchase a given item in a 
+given quantity, such as five boxes of tissues. 
+You might wish to implement bulk-discounted items, 
+uch as two boxes of tissues for $4, which would bring 
+the cost of the given item order of 2+2+1
+----
+Methods to implement: 
+
+*/ 
 
 public class ItemOrder {
-
-	public ItemOrder() {
-		// TODO Auto-generated constructor stub
-	}
 
 }
