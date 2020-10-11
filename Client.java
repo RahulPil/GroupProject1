189e55f4548e0e6b1a4fca23653780f22ecc44df
sheros21 <71781274+sheros21@users.noreply.github.com>
2020-10-11 16:18:43 -0700

    Added in add/remove functions

--- Client.java
+++ Client.java
@@ -27,42 +27,100 @@ public class Client {
 		System.out.println("--------------");
 		String input = "";
 		while(input.equals("exit") == false){
+         System.out.print("input: ");
 			String userinput = scan.nextLine();
-			String input = userinput.toLowerCase();
+			input = userinput.toLowerCase();
 			System.out.println("You have entered: " + input);
-			itemsList.add(input);
-			
-			switch(input){
-				case "apple":
-				itemsList.add(apple.getName());
-				break;
-
-				case "banana":
-				itemsList.add(banana.getName());
-				break;
-
-				case "broccoli":
-				itemsList.add(broccoli.getName());
-				break;
-
-				case "carrot":
-				itemsList.add(carrot.getName());
-				break;
-
-				case "oats":
-				itemsList.add(oats.getName());
-				break;
-
-				case "potato":
-				itemsList.add(potato.getName());
-				break;
-
-				case "zucchini":
-				itemsList.add(zucchini.getName());
-				break;
+         
+         //itemsList.add(input);
+			if (input.equals("add")){
+            System.out.print("enter in item name: ");
+            userinput = scan.nextLine();
+			   input = userinput.toLowerCase();
+            System.out.println("You have entered: " + (input));
+   			   switch(input){
+   				case "apple":
+   				itemsList.add(apple.getName());
+   				break;
+   
+   				case "banana":
+   				itemsList.add(banana.getName());
+   				break;
+   
+   				case "broccoli":
+   				itemsList.add(broccoli.getName());
+   				break;
+   
+   				case "carrots":
+   				itemsList.add(carrots.getName());
+   				break;
+   
+   				case "oatmeal":
+   				itemsList.add(oats.getName());
+   				break;
+   
+   				case "potato":
+   				itemsList.add(potatos.getName());
+   				break;
+   
+   				case "zucchini":
+   				itemsList.add(zucchini.getName());
+   				break; 
+            }
 			}
+         else if (input.equals("remove")){
+            System.out.print("enter in item name: ");
+            userinput = scan.nextLine();
+			   input = userinput.toLowerCase();
+            System.out.println("You have entered: " + (input));
+            switch(input){
+   				case "apple":
+   				itemsList.remove(apple.getName());
+   				break;
+   
+   				case "banana":
+   				itemsList.remove(banana.getName());
+   				break;
+   
+   				case "broccoli":
+   				itemsList.remove(broccoli.getName());
+   				break;
+   
+   				case "carrots":
+   				itemsList.remove(carrots.getName());
+   				break;
+   
+   				case "oatmeal":
+   				itemsList.remove(oats.getName());
+   				break;
+   
+   				case "potato":
+   				itemsList.remove(potatos.getName());
+   				break;
+   
+   				case "zucchini":
+   				itemsList.remove(zucchini.getName());
+   				break; 
+            }
+               
+         }
+         else if (input == "stocklist"){
+            System.out.println("stock: ");
+            
+            break;
+         }
+         
+         else{
+            System.out.println("invalid input");
+            
+         }
+         
 		}
-		System.out.println("You have exited");
+      System.out.println("You have exited");
+      System.out.println();
+      System.out.println("Recipt:");
+      System.out.println(itemsList);
+      
 		scan.close();
 	}
 }
