79ede43059750c9a56389d814d1d0f93d6a09906
sheros21 <71781274+sheros21@users.noreply.github.com>
2020-10-12 15:50:38 -0700

    Update ItemOrder.java
    Updated comments and header block

--- ItemOrder.java
+++ ItemOrder.java
@@ -1,46 +1,53 @@
 /* 
-... also implement an ItemOrder class that represents a 
-shopper’s desire to purchase a given item in a 
-given quantity, such as five boxes of tissues. 
-You might wish to implement bulk-discounted items, 
-uch as two boxes of tissues for $4, which would bring 
-the cost of the given item order of 2+2+1
-----
-Methods to implement: 
+ * Group Project #1
+ * Group 6 (Anders, Julian, Rahul, Rosie, Sean, Vatsal)
+ * CS 211
+ * Due: 10/12/2020
+ * Quarter: Fall 2020
+ * 
+ * ItemOrder Class creates a ItemOrder object that represents a shopper's 
+ * order in purchasing a item in a given quantity. 
 
 */ 
 
 public class ItemOrder {
+	// field
 	private Item item;
 	private int count;
 	
-	//CONSTRUCTORS
+	// CONSTRUCTORS
 	ItemOrder(Item item, int count) {
 		this.item = item;
 		this.count = count;
 	}
+	
+	// deafult constructor
 	ItemOrder(Item item) {
 		this(item, 1);
 	}
 	
-	//SETTERS
+	// SETTERS
+	// mutator method sets the ItemOrder object's Item
 	public void setItem(Item item) {
 		this.item = item;
 	}
-	
+	// mutator method sets the ItemOrder object's quantity
 	public void setCount(int count) {
 		this.count = count;
 	}
 	
-	//GETTERS
+	// GETTERS
+	// accessor for ItemOrder object's Item
 	public Item getItem() {
 		return item;
 	}
 	
+	// accessor for ItemOrder object's quantity
 	public int getCount() {
 		return count;
 	}
 	
+	// calculates and returns the ItemOrder's price
 	public double getOrderCost() {
 		if(item.getBulkCount() <= count) return item.getDiscountedPrice()*count;
 		return item.getPrice()*count;
